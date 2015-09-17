7eea1306a115348e67dd6126ab0aba3a7da266b9
Mattis Kancans Envall <mattiskan@gmail.com>
2015-09-17 21:06:26 +0000

    working dev server

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');  
+var path = require('path');
+
+module.exports = {  
+ entry: [
+   'webpack-dev-server/client?http://localhost:8080',
+   'webpack/hot/only-dev-server',
+   './src/index.js'
+ ],
+ module: {
+   loaders: [{
+     test: /\.jsx?$/,
+     exclude: /node-modules/,
+     include: path.join(__dirname, "src"),
+	
+     loader: 'react-hot!babel'
+   }]
+ },
+ resolve: {
+   extensions: ['', '.js', '.jsx']
+ },
+ output: {
+   path: __dirname + '/dist',
+   publicPath: '/',
+   filename: 'bundle.js'
+ },
+ devServer: {
+  contentBase: './dist',
+   hot: true
+ },
+ plugins: [
+   new webpack.HotModuleReplacementPlugin()
+ ]
+};
